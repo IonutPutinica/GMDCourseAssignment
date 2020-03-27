26ef616c413d4bf790cbf91201055d7ebf88abe3
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-03-27 17:15:10 +0100

    Added Camera Controller Script

--- New Unity Project/Assets/Scripts/ThirdPersonCameraController.cs
+++ New Unity Project/Assets/Scripts/ThirdPersonCameraController.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class ThirdPersonCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY  = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
     }
 }
